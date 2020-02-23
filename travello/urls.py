cd1def12745df4fcd38a621ffb41762ad777d402
darvel <darusohrid@yahoo.com>
2020-02-23 19:36:19 +0100

    Authentication added

--- travello/urls.py
+++ travello/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.travello, name='home')
+    path('', views.travello, name='home'),
+    path('destination', views.destination, name='destination')
 ]
--- travello/views.py
+++ travello/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Destination
 
 # Create your views here.
@@ -7,3 +7,14 @@ def travello(request):
     destinations = Destination.objects.all()
 
     return render(request, 'index.html', {'dests': destinations})
+
+def destination(request):
+
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return render(request, 'destinations.html')
+        else:
+            return redirect('accounts/login')
+
+
+
